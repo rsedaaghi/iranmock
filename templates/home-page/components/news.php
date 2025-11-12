8f4a3791c1aa212ed5dcd8d483759d2da1fc05af
Reza Sedaaghi <rsedaaghi@gmail.com>
2025-11-12 12:26:28 +0330

    UPDATE:

--- templates/home-page/components/news.php
+++ templates/home-page/components/news.php
@@ -16,34 +16,53 @@ $news_query = new WP_Query([
 
     <!-- Desktop Grid -->
     <div class="row justify-content-center d-none d-md-flex">
-        <?php if ($news_query->have_posts()): ?>
-            <?php while ($news_query->have_posts()): $news_query->the_post(); ?>
-                <div class="col-auto mb-4">
-                    <div class="card text-center h-100 custom-news-card">
-                        <div class="card-img-wrapper">
-                            <img src="<?= esc_url(get_the_post_thumbnail_url(get_the_ID(), 'medium')) ?>" class="card-img-top"
-                                alt="<?= esc_attr(get_the_title()) ?>">
+        <div class="col-12 news-box-wrapper">
+            <!-- Card container using flexbox -->
+            <div class="d-flex flex-wrap justify-content-center gap-4">
+                <?php if ($news_query->have_posts()): ?>
+                    <?php
+                    $count = 0;
+                    while ($news_query->have_posts() && $count < 6): $news_query->the_post();
+                    ?>
+                        <div class="custom-news-card-wrapper mb-4">
+                            <div class="card text-center custom-news-card">
+                                <div class="card-img-wrapper">
+                                    <img src="<?= esc_url(get_the_post_thumbnail_url(get_the_ID(), 'medium')) ?>"
+                                        class="card-img-top" alt="<?= esc_attr(get_the_title()) ?>">
+                                </div>
+                                <div class="card-body">
+                                    <p class="news-label mb-2 make-block"><?= esc_html(get_the_title()) ?></p>
+                                    <p class="news-description mt-1 mb-3">
+                                        <?= esc_html(get_the_excerpt()) ?>
+                                    </p>
+                                    <a href="<?= esc_url(get_permalink()) ?>" class="btn btn-success mb-3">
+                                        <?= esc_html(iranmock_translate('view')) ?>
+                                    </a>
+                                </div>
+                            </div>
                         </div>
-                        <div class="card-body">
-                            <p class="news-label mb-2"><?= esc_html(get_the_title()) ?></p>
-                            <?php
-                            $excerpt = get_the_excerpt();
-                            $trimmedExcerpt = mb_strlen($excerpt) > $excerptMaxLength
-                                ? mb_substr($excerpt, 0, $excerptMaxLength) . '...'
-                                : $excerpt;
-                            ?>
-                            <p class="news-description my-3"><?= esc_html($trimmedExcerpt) ?></p>
-                            <a href="<?= esc_url(get_permalink()) ?>" class="btn btn-success mb-3">
-                                <?= esc_html(iranmock_translate('view')); ?>
-                            </a>
-                        </div>
+                    <?php
+                        $count++;
+                    endwhile;
+                    wp_reset_postdata();
+                    ?>
+                <?php else: ?>
+                    <div class="col-12">
+                        <p class="text-center">No news found in this category.</p>
                     </div>
+                <?php endif; ?>
+            </div>
+
+            <!-- Second row: left-aligned button -->
+            <div class="row m2-4">
+                <div class="col text-start">
+                    <a href="<?= esc_url(get_category_link(get_category_by_slug('news')->term_id)) ?>"
+                        class="btn btn-link see-all-btn px-0">
+                        <?= esc_html(iranmock_translate('see_all') . ' ' . iranmock_translate('news')) ?>
+                    </a>
                 </div>
-            <?php endwhile;
-            wp_reset_postdata(); ?>
-        <?php else: ?>
-            <p class="text-center">No news found in this category.</p>
-        <?php endif; ?>
+            </div>
+        </div>
     </div>
 
     <!-- Mobile Scroll -->
@@ -77,12 +96,13 @@ $news_query = new WP_Query([
                 wp_reset_postdata(); ?>
             <?php endif; ?>
         </div>
+        <div class="d-flex justify-content-end mt-3">
+            <a href="<?= esc_url(get_category_link(get_category_by_slug('news')->term_id)) ?>"
+                class="btn btn-link see-all-btn">
+                <?= esc_html(iranmock_translate('see_all') . ' ' . iranmock_translate('news')); ?>
+            </a>
+        </div>
     </div>
 
-    <div class="d-flex justify-content-end mt-3">
-        <a href="<?= esc_url(get_category_link(get_category_by_slug('news')->term_id)) ?>"
-            class="btn btn-link see-all-btn">
-            <?= esc_html(iranmock_translate('see_all') . ' ' . iranmock_translate('news')); ?>
-        </a>
-    </div>
+
 </div>
